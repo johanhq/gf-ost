085fe46cf03bb06866b3996e2600042f4f7a0369
Johan Hörnqvist <johan.hornqvist@kry.se>
2020-03-05 10:48:36 +0100

    First build

---
+++ build/precache-manifest.2fa39ea84c835cffcb8e3b54bfbebb3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3022f5f33073fb3334218243beb0839",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e606068ad32591f923ac",
+    "url": "/static/css/2.6d7d54d2.chunk.css"
+  },
+  {
+    "revision": "f4d81db4a77624d544c9",
+    "url": "/static/css/main.13d18976.chunk.css"
+  },
+  {
+    "revision": "e606068ad32591f923ac",
+    "url": "/static/js/2.f4a96a8f.chunk.js"
+  },
+  {
+    "revision": "9b499988666cc8d7d52d70a8c3b2f162",
+    "url": "/static/js/2.f4a96a8f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f4d81db4a77624d544c9",
+    "url": "/static/js/main.dfe7798a.chunk.js"
+  },
+  {
+    "revision": "29dfe06cf969acb9f4b0",
+    "url": "/static/js/runtime-main.ac580c33.js"
+  }
+]);
